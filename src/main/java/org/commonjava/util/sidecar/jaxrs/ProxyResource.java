6ecbd9bd400cf26a8f36363977e8562038348d03
yma <yma@redhat.com>
2021-04-23 21:07:00 +0800

    Keep ProxyResource for non-folo proxy

---
+++ src/main/java/org/commonjava/util/sidecar/jaxrs/ProxyResource.java
@@ -0,0 +1,84 @@
+/**
+ * Copyright (C) 2011-2021 Red Hat, Inc. (https://github.com/Commonjava/indy-sidecar)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.commonjava.util.sidecar.jaxrs;
+
+import io.smallrye.mutiny.Uni;
+import io.vertx.core.http.HttpServerRequest;
+import org.commonjava.util.sidecar.services.ProxyService;
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.HEAD;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import java.io.InputStream;
+
+@Path( "/{path: (.*)}" )
+public class ProxyResource
+{
+    private final Logger logger = LoggerFactory.getLogger( getClass() );
+
+    @Inject
+    ProxyService proxyService;
+
+    @HEAD
+    public Uni<Response> head( @PathParam( "path" ) String path, final @Context HttpServerRequest request )
+                    throws Exception
+    {
+        logger.debug( "Head resource: {}", path );
+        return proxyService.doHead( path, request );
+    }
+
+    @GET
+    public Uni<Response> get( @PathParam( "path" ) String path, final @Context HttpServerRequest request )
+                    throws Exception
+    {
+        logger.debug( "Get resource: {}", path );
+        return proxyService.doGet( path, request );
+    }
+
+    @POST
+    public Uni<Response> post( @PathParam( "path" ) String path, InputStream is,
+                               final @Context HttpServerRequest request ) throws Exception
+    {
+        logger.debug( "Post resource: {}", path );
+        return proxyService.doPost( path, is, request );
+    }
+
+    @PUT
+    public Uni<Response> put( @PathParam( "path" ) String path, InputStream is,
+                              final @Context HttpServerRequest request ) throws Exception
+    {
+        logger.debug( "Put resource: {}", path );
+        return proxyService.doPut( path, is, request );
+    }
+
+    @DELETE
+    public Uni<Response> delete( @PathParam( "path" ) String path, final @Context HttpServerRequest request )
+                    throws Exception
+    {
+        logger.debug( "Delete resource: {}", path );
+        return proxyService.doDelete( path, request );
+    }
+
+}
