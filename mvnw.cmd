bfe23f6e6d4645a347538dc404e78a2bfdec7b06
Gang Li <gli@redhat.com>
2022-11-08 01:18:50 +0000

    Change inception year and formatting header

--- mvnw.cmd
+++ mvnw.cmd
@@ -1,5 +1,5 @@
 @REM
-@REM Copyright (C) 2011-2021 Red Hat, Inc. (https://github.com/Commonjava/indy-sidecar)
+@REM Copyright (C) 2011-2022 Red Hat, Inc. (https://github.com/Commonjava/indy-sidecar)
 @REM
 @REM Licensed under the Apache License, Version 2.0 (the "License");
 @REM you may not use this file except in compliance with the License.
